eadc3e8dc9e20358c8a8f38cda88559076e73ee9
lahkok <masaidahmadirfan@gmail.com>
2023-11-01 08:21:59 +0700

    some ui improvements, adding user and password for login implementation, creating new activity such as forgot password and homepage and more

--- app/src/main/java/com/example/tugasmobile/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/tugasmobile/ForgotPasswordActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_forgot_password);
     }
 }
